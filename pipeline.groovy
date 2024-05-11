82bca26e66b509dbf6f3cee42ca2e7703db6ecc4
sumit123-456 <96869106+sumit123-456@users.noreply.github.com>
2024-05-11 15:39:54 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,9 +6,9 @@ pipeline {
                 git branch: 'main', credentialsId: '06778837-c2de-4c34-bf8a-d60ea48c811b', url: 'https://github.com/sumit123-456/jenkins-cicd.git'
             }
         }
-         stage('Build') {
+         stage('pull') {
             steps {
-                echo "build successful"
+                echo "pull successful"
             }
          }
         stage('Test') {
